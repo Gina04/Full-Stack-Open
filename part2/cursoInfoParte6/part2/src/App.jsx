4138a60b22931ebfbe8191bb3adfe96dfbb71f39
Gina04 <gbosque@frba.utn.edu.ar>
2024-12-26 10:12:39 -0300

    Cursoinfo step 9

--- part2/cursoInfoParte6/part2/src/App.jsx
+++ part2/cursoInfoParte6/part2/src/App.jsx
@@ -49,7 +49,7 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
+  /*const course = {
     id: 1,
     name: "Half Stack application development",
     parts: [
@@ -69,9 +69,60 @@ const App = () => {
         id: 3,
       },
     ],
-  };
+  };*/
 
-  return <Course course={course} />;
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      {courses.map((course)=>(
+        <Course key= {courses.id} course={course} />
+      ))}
+    </div>
+  )
 };
 
 export default App;
