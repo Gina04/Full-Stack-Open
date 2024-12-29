b0772eaac65943f813ad10ef60cc0489dd187430
Gina04 <gbosque@frba.utn.edu.ar>
2024-12-29 01:05:04 -0300

    the phone book step 6

--- part2/thePhoneBook/part2/src/App.jsx
+++ part2/thePhoneBook/part2/src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PersonList from "./components/PersonList";
 import PersonForm from "./components/PersonForm";
 import FilterNamePerson from "./components/FilterNamePerson";
+import axios from 'axios'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [showAll, setShowAll] = useState("");
+
+  useEffect(()=>{
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response =>{
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+   },[])
+   console.log('render', persons.length, 'notes')
+
 
   const addPhoneBook = (event) => {
     event.preventDefault();
